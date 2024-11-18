5f78fcb059bc63fef1d8ad6a705436c41220d232
Bex Edmondson <bexedmondson@gmail.com>
2024-11-18 02:35:11 +0000

    collar component first attempt

---
+++ src/CollarPath.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let h = 100;
+    export let w = 100;
+    export let type = "round";
+</script>
+
+{#if (type === "round")}
+<path
+    d= "M {w/2-w/8},{h/8} 
+        C {w/2-w/8},{h/4-h/16} {w/2-w/16},{h/4} {w/2},{h/4}
+        C {w/2+w/16},{h/4} {w/2+w/8},{h/4-h/16} {w/2+w/8},{h/8}
+       "
+    fill="none"
+    stroke="black"
+    stroke-width="1px"
+/>
+{:else}
+<path
+    d= "M {w/2-w/8},{h/8} 
+        C {w/2-w/8},{h/4-h/16} {w/2-w/16},{h/4} {w/2},{h/4}
+        C {w/2+w/16},{h/4} {w/2+w/8},{h/4-h/16} {w/2+w/8},{h/8}
+       "
+    fill="none"
+    stroke="red"
+    stroke-width="1px"
+/>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
+<script>
+	import { draw } from 'svelte/transition';
+    import '$lib/points.js';
+    import CollarPath from "../CollarPath.svelte";
+
+    let collarType = "round";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={ (clicked) => {
+                        if (collarType === "round") 
+                            collarType = "flat";
+                        else
+                            collarType = "round";
+                        }
+                }>
+{collarType}
+</button>
+
+<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    {#key collarType}
+    <CollarPath h={100} w={100} type={collarType} />
+    {/key}
+</svg>
+
+<style>
+	svg {
+		display: block;
+		height: 50%;
+		width: 50%;
+	}
+</style>
