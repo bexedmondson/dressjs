0b185766cca33b46ccc1a0572875dad6dba34164
Bex Edmondson <bexedmondson@gmail.com>
2024-11-24 19:09:05 +0000

    formatting

--- src/lib/collars.js
+++ src/lib/collars.js
@@ -10,7 +10,11 @@ export function getCollarCrew(left, right)
     let my = (ry + ly) / 2 + radius;
     let tgnt = radius / 2;
 
-    return `M ${lx},${ly} L ${lx+radius},${ly} C ${lx+radius},${ly+tgnt} ${mx-tgnt},${my} ${mx},${my} C ${mx+tgnt},${my} ${rx-radius},${ry+tgnt} ${rx-radius},${ry} L ${rx},${ry}`;
+    return `M ${lx},${ly} 
+    L ${lx+radius},${ly} 
+    C ${lx+radius},${ly+tgnt} ${mx-tgnt},${my} ${mx},${my} 
+    C ${mx+tgnt},${my} ${rx-radius},${ry+tgnt} ${rx-radius},${ry} 
+    L ${rx},${ry}`;
 }
 
 export function getCollarScoop(left, right)
@@ -25,12 +29,15 @@ export function getCollarScoop(left, right)
     let my = (ry + ly) / 2 + radius;
     let tgnt = radius / 2;
 
-    return `M ${lx},${ly} C ${lx},${ly+tgnt} ${mx-tgnt},${my} ${mx},${my} C ${mx+tgnt},${my} ${rx},${ry+tgnt} ${rx},${ry}`;
+    return `M ${lx},${ly} 
+    C ${lx},${ly+tgnt} ${mx-tgnt},${my} ${mx},${my} 
+    C ${mx+tgnt},${my} ${rx},${ry+tgnt} ${rx},${ry}`;
 }
 
 export function getCollarFlat(left, right)
 {
-    return `M ${left[0]},${left[1]} L ${right[0]},${right[1]}`
+    return `M ${left[0]},${left[1]} 
+    L ${right[0]},${right[1]}`
 }
 
 export function getCollarV(left, right)
@@ -44,7 +51,9 @@ export function getCollarV(left, right)
     let mx = (rx + lx) / 2;
     let my = (ry + ly) / 2 + radius;
 
-    return `M ${lx},${ly} L ${mx},${my} L ${rx},${ry}`;
+    return `M ${lx},${ly} 
+    L ${mx},${my} 
+    L ${rx},${ry}`;
 }
 
 export function getCollarBoat(left, right)
@@ -60,5 +69,7 @@ export function getCollarBoat(left, right)
     let mx = (rx + lx) / 2;
     let my = (ry + ly) / 2 + ytgnt;
 
-    return `M ${lx},${ly} C ${lx+xtgnt},${ly+ytgnt} ${mx-ytgnt},${my} ${mx},${my} C ${mx+ytgnt},${my} ${rx-xtgnt},${ry+ytgnt} ${rx},${ry}`;
+    return `M ${lx},${ly} 
+    C ${lx+xtgnt},${ly+ytgnt} ${mx-ytgnt},${my} ${mx},${my} 
+    C ${mx+ytgnt},${my} ${rx-xtgnt},${ry+ytgnt} ${rx},${ry}`;
 }
